de3a89d93602dfde399cd9fb6ef066b11538ef51
Trever <t.hixson08@gmail.com>
2014-02-01 14:57:39 -0700

    Bootstrap

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
+<!DOCTYPE html>
 <html>
-<head>
-  <title>CS 462 - Trever Hixson</title>
-</head>
-<body>
-  <b>Trever Hixson</b>
-</body>
+  <head>
+    <title>CS 462 - Trever Hixson</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
