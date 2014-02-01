2bcde19026341fe23f9e5bc8db945a0a458d3b56
Trever <t.hixson08@gmail.com>
2014-02-01 15:29:06 -0700

    Sign in and Register pages

--- index.php
+++ index.php
@@ -1,14 +1,30 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>CS 462 - Trever Hixson</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+<head>
+	<title>Home</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<a class="brand" href="/">CS 462</a>
+		</div>
+		<div class="nav-collapse collapse navbar-responsive-collapse">
+			<ul class="nav" id="topLinks">
+				<li class="active"><a href="/">Home</a></li>
+				<li><a href="/signin.php">Sign In</a></li>
+				<li><a href="/register.php">Register</a></li>
+			</ul>
+		</div>
+	</div><!-- navbar -->
+	
+	<div class="container content">
+		<b>Trever Hixson</b>
+	</div><!-- container content -->
+	
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
 </html>
---
+++ register.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<style>
+		#signIn{
+			max-width: 300px;
+			margin: 0 auto;
+			border: 1px solid grey;
+			padding: 15px;
+			margin-top: 15px;
+			border-radius: 5px;
+		}
+		#signInFields{
+			max-width: 220px;
+			margin: 0 auto;
+		}
+		#signInFields button{
+			float: left;
+		}
+		#signInFields span{
+			padding-top: 10px;
+			float: left;
+		}
+	</style>
+</head>
+<body>
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<a class="brand" href="/">CS 462</a>
+		</div>
+		<div class="nav-collapse collapse navbar-responsive-collapse">
+			<ul class="nav" id="topLinks">
+				<li><a href="/">Home</a></li>
+				<li><a href="/signin.php">Sign In</a></li>
+				<li class="active"><a href="/register.php">Register</a></li>
+			</ul>
+		</div>
+	</div><!-- navbar -->
+	
+	<div class="container content">
+		<div id="signIn">
+			<form method="POST" action="/PHPForms/register.php" id="signInForm">
+				<fieldset>
+					<legend>Register</legend>
+					<div id="signInFields">
+						<label>Username</label>
+						<input type="text" id="username" name="username">
+						<div id="usernameMessage"></div>
+						<button type="submit" class="btn">Register</button>
+					</div>
+				</fieldset>
+			</form>
+		</div>
+	</div><!-- container content -->
+	
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
