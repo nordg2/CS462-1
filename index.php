e40741e368a7099f410ca6b3edcb3f7591ae4807
Trever <t.hixson08@gmail.com>
2014-02-04 08:34:41 -0700

    lab 1 updates

--- index.php
+++ index.php
@@ -11,23 +11,57 @@
 		#content{
 			padding-top: 15px;
 		}
+		.sidenav{
+			border: 1px solid gray;
+			border-radius: 5px;
+			margin-bottom: 10px;
+		}
 	</style>
 </head>
 <body>
 	<?php include "pageParts/navbar.php" ?>
 	
 	<div class="container content" id="content">
-		<h3><u>Current Users</u></h3>
-		<?php
-		//Load in the current users
-		$file = "forms/users.json";
-		$string = file_get_contents($file);
-		$users=json_decode($string,true);
+		<div class="row-fluid">
+			<div class="span2">
+				<ul class="nav nav-list affix-top sidenav" id="tagList">
+					<li class="nav-header">Current Users</li>
+					<?php
+					//Load in the current users
+					$file = "forms/users.json";
+					$string = file_get_contents($file);
+					$users=json_decode($string,true);
+					
+					if(isset($_GET['user'])){
+						$username = $_GET['user'];
+					}
+					else{
+						$username = "";
+					}
+					foreach($users as $name){
+						if($username == $name){
+							echo '<li class="active"><a href="index.php?user=' . $name . '">' . $name . '</a></li>';
+						}
+						else{
+							echo '<li><a href="index.php?user=' . $name . '">' . $name . '</a></li>';
+						}
+					}
+					?>
+				</ul>
+			</div>
 		
-		foreach($users as $name){
-			echo $name . '<br/>';
-		}
-		?>
+			<div class="span10">
+				<?php
+				if($username == ""){
+					echo "Click a user to see their last check-in location.";
+				}
+				else{
+					echo $username . " data";
+				}
+				?>
+			</div>
+
+		</div>
 	</div><!-- container content -->
 	
 	<script src="js/bootstrap.min.js"></script>
