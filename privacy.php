aa50e2ecc7649fd55b86d63a5619cc0b9ff58e0a
Trever <t.hixson08@gmail.com>
2014-02-05 10:50:41 -0700

    Load jquery by https

--- privacy.php
+++ privacy.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- Bootstrap -->
 	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="https://code.jquery.com/jquery.js"></script>
 	<style>
 		#content{
 			padding-top: 15px;
